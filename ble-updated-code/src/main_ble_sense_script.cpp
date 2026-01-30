a31a224a869788350867c986e76c509a9cdbe66a
Adithya <adithya.suresh2021@vitstudent.ac.in>
2026-01-30 11:23:48 +0000

    troubleshooting code added

--- ble-updated-code/src/main_ble_sense_script.cpp
+++ ble-updated-code/src/main_ble_sense_script.cpp
@@ -17,9 +17,15 @@ struct imuData {
 void setup() {
   // Serial initialisations
   Serial.begin(115200);   // USB serial monitor
+  delay(1000);
   Serial1.begin(9600);    // UART from Uno
 
   pinMode(LED_BUILTIN, OUTPUT); // Built in LED for debugging
+
+   //debugging purposes
+  Serial.println("BLE Sense booted");
+    //header for the python code
+  Serial.println("t_ms,ax,ay,az,r,p,y,avx,avy,avz"); 
 }
 
 void loop() {

3a3cefde74983ecf5b1b338d0da3d7d8c410daa9
Adithya <adithya.suresh2021@vitstudent.ac.in>
2026-03-04 16:57:31 +0000

    added comments to logging file, updated inferencce file to stop once cell is interrupted

--- Datapipeline/log_serial_to_csv_live.py
+++ Datapipeline/log_serial_to_csv_live.py
@@ -1,5 +1,5 @@
 #run file while keeping in mind the COM port to which arduino is connected to 
-#python3 src/log_serial_to_csv_live.py <USB_Com_Port>
+# python3 Datapipeline/log_serial_to_csv_live.py /dev/cu.usbmodem<COM_PORT_HERE>
 
 import csv
 import sys
